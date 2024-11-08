78cda9fbe2998ae11c9cdc517fa342266bffa1f1
Carifio24 <carifio24@gmail.com>
2024-11-08 17:40:57 -0500

    Title-case power plant types in subset widget.

--- src/tempods/components/subset_control_widget/subset_control_widget.vue
+++ src/tempods/components/subset_control_widget/subset_control_widget.vue
@@ -20,7 +20,7 @@
             <v-list-item-content
               class="font-weight-bold"
             >
-              {{ option }}
+              {{ toTitleCase(option) }}
             </v-list-item-content>
             <v-list-item-action>
               <v-checkbox
@@ -52,7 +52,7 @@
             <v-list-item-action>
               <v-checkbox
                 :input-value="active"
-                :color="gray"
+                color="gray"
               />
             </v-list-item-action>
           </template>
@@ -61,3 +61,16 @@
     </v-list>
   </v-card>
 </template>
+
+<script>
+export default {
+  methods: {
+    toTitleCase(str) {
+      return str.replace(
+        /\w\S*/g,
+        text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+      );
+    }
+  }
+}
+</script>
